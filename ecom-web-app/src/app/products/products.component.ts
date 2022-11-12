1069280f43262c43ec8f8a9abef3388f196fbb5d
Khaoula Elmajni <elmajnikhaoula99@gmail.com>
2022-11-12 15:00:39 +0100

    frontend angular

---
+++ ecom-web-app/src/app/products/products.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products : any;
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:9999/inventory-service/products?projection=fullProduct").subscribe({
+      next : (data)=>{
+        this.products = data;
+      },
+      error : (err)=>{
+
+      }
+    })
+  }
+
+}
